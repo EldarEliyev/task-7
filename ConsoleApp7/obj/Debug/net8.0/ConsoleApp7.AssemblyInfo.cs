b94298b62ac0af3d96cb301852138af74a0bb22b
eldar23451 <liyev8024@gmail.com>
2024-05-16 03:52:27 +0400

    task file is added

--- ConsoleApp7/obj/Debug/net8.0/ConsoleApp7.AssemblyInfo.cs
+++ ConsoleApp7/obj/Debug/net8.0/ConsoleApp7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e629b72b49495b1525f9c3a76b85575f3842b80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
